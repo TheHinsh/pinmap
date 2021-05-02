84e773a68de7e0b2a342cad42f7e485e2b0af022
Chris_Hinshaw <chris.hinshaw@packetsled.com>
2021-05-01 21:22:22 -0700

    Add command line options

--- create_map.py
+++ create_map.py
@@ -1,26 +1,49 @@
+import argparse
+import sys
+
 from staticmap import StaticMap, CircleMarker
 from geopy.geocoders import Nominatim
 from pprint import pprint
 
-geolocator = Nominatim(user_agent="Please_Edit_This_Text")
+url_template = 'http://a.tile.osm.org/{z}/{x}/{y}.png'
 
-m = StaticMap(200, 200, url_template='http://a.tile.osm.org/{z}/{x}/{y}.png')
-home_lat = 47.412279
-home_lon = -122.692318
-coordinates = [(home_lon,home_lat)]
-with open("address.lst") as f:
-    lines = f.readlines()
-    pprint(lines)
-    for line in lines:
-        address = line.strip()
-        pprint(address)
-        location = geolocator.geocode(address)
-        pprint(location)
-        coordinates.append((location.longitude, location.latitude))
-pprint(coordinates)
-for coord in coordinates:
-    marker = CircleMarker(coord, '#0036FF', 12)
-    m.add_marker(marker)
+def main(argv):
+    parser = argparse.ArgumentParser(
+        prog='create_map',
+    )
 
-image = m.render(zoom=8)
-image.save('marker.png')
+    arg_group = parser.add_argument_group('Arguments')
+    arg_group.add_argument('--verbose', '-v', action='count', help='Set verbose output', default=0)
+    arg_group.add_argument('-i', help="Address List Table (address.lst)", type=str, dest="infile", default='address.lst')
+    arg_group.add_argument('-o', help="PNG file to create (marker.png)", type=str, dest="outfile", default='marker.png')
+    arg_group.add_argument('-n', help="Your/Company Name (default)", type=str, dest="name", default='default')
+    arg_group.add_argument("--img_w", help="Width of the output image (400)", type=int, dest="width", default=400)
+    arg_group.add_argument("--img_h", help="Height of the output image (400)", type=int, dest="height", default=400)
+    arg_group.add_argument('-z', help="Image Zoom factor (10)", type=int, dest="zoom", default=10)
+
+    args = parser.parse_args(argv)
+
+    home_lat = 47.412279
+    home_lon = -122.692318
+
+    m = StaticMap(width=args.width, height=args.height, url_template=url_template)
+    m.add_marker(CircleMarker((home_lon, home_lat), 'blue', 12))
+    with open(args.infile) as f:
+        lines = f.readlines()
+        geolocator = Nominatim(user_agent=args.name)
+        pprint(lines)
+        for line in lines:
+            parts = line.strip().split(':')
+            address = parts[0]
+            color = parts[1] if len(parts) > 1 else 'blue'
+            cm_w = int(parts[2]) if len(parts) > 2 else 12
+            pprint([address, color, cm_w])
+            location = geolocator.geocode(address)
+            pprint(location)
+            m.add_marker(CircleMarker((location.longitude, location.latitude), color, cm_w))
+
+    image = m.render(zoom=args.zoom)
+    image.save('marker.png')
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
